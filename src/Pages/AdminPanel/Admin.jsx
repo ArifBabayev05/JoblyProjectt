cfeaec064d38f315c072eb2bd088f89dad68f65e
Arif Babayev <arifrb@code.edu.az>
2022-09-10 12:47:46 +0400

    Add Tailwind

--- src/Pages/AdminPanel/Admin.jsx
+++ src/Pages/AdminPanel/Admin.jsx
@@ -4,23 +4,17 @@ import ErrorPage from '../UserPanel/ErrorPage'
 import './Admin.css'
 import { Link } from 'react-router-dom'
 import './Admin.css'
-import Sidebar from './components/Sidebar/Sidebar'
-import MainDash from './components/MainDash/MainDash'
-import RightSide from './components/RightSide/RightSide'
-
-
 function Admin() {
     return (
         <div>
 
             <ShowOnAdmin>
-                <div className='Dashboard'>
-                    <div className='AppGlass'>
-
-                    <Sidebar/>
-                    <MainDash/>
-                    <RightSide/>
+                <div>
+                    <div>Sidebar</div>
+                    <div className='p-7 text-2x1 font-semibold'>
+                    <h1>Home</h1>
                     </div>
+                    
 
                 </div>
 
