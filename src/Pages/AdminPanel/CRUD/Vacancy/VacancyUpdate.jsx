aa47cd81af2dd113957173c554bb01cbb89fa165
Arif Babayev <arifrb@code.edu.az>
2022-09-13 10:33:54 +0400

    Default values

--- src/Pages/AdminPanel/CRUD/Vacancy/VacancyUpdate.jsx
+++ src/Pages/AdminPanel/CRUD/Vacancy/VacancyUpdate.jsx
@@ -29,7 +29,6 @@ const VacancyUpdate = (props) => {
   function submit(e) {
     e.preventDefault();
     console.log(data);
-    
     axios.post(`http://localhost:53410/api/Vacancies/update?id=${id}`, {
       id: data.id,
       name: data.name,
@@ -50,7 +49,7 @@ const VacancyUpdate = (props) => {
       toast.error("Əməliyyat Uğursuzdur.");
     })
   }
-  
+
   function handle(e) {
 
     const newData = { ...data }
@@ -60,8 +59,126 @@ const VacancyUpdate = (props) => {
     setData(newData);
   }
 
-  console.log(data);
-  
+
+  const url = `http://localhost:53410/api/Vacancies/getbyid?id=${id}`
+  const [product, setProduct] = useState({
+    loading: false,
+    data: null,
+    error: false,
+    
+    
+  })
+
+  let content = null
+
+  useEffect(() => {
+    setProduct({
+      loading: true,
+      data: null,
+      error: false
+
+    })
+
+    axios.get(url)
+      .then(response => {
+        setProduct({
+          loading: false,
+          data: response.data,
+          error: false
+        })
+
+          .catch(() => {
+            setProduct({
+              loading: false,
+              data: null,
+              error: true
+            })
+          })
+      })
+  }, [url])
+
+  if (product.loading) {
+    content = <Loader />
+  }
+
+  if (product.error) {
+    content = <p>Xəta baş verdi, yenidən yoxlayın.</p>
+  }
+
+  if (product.data) {
+    content =
+      <form encType='multipart/formdata' onSubmit={(e) => submit(e)}>
+        {/* {loading && <Loader />} */}
+
+
+
+        <input defaultValue={data.id} onChange={(e) => handle(e)} value={data.value} style={{ 'display': 'none' }} type="name" required class="form-control" id="id" placeholder="Ad" />
+
+        <div class="row mb-3">
+          <label for="inputEmail" class="col-sm-2 col-form-label">Ad</label>
+          <div class="col-sm-10">
+            <input defaultValue={product.data.name} onChange={(e) => handle(e)} value={data.value} type="name" required class="form-control" id="name" placeholder="Ad" />
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <label for="inputEmail" class="col-sm-2 col-form-label">typeOfwork</label>
+          <div class="col-sm-10">
+            <input defaultValue={product.data.typeOfwork} onChange={(e) => handle(e)} value={data.value} type="name" required class="form-control" id="typeOfwork" placeholder="typeOfwork" />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="inputEmail" class="col-sm-2 col-form-label">vəzifəÖhdəlikləri</label>
+          <div class="col-sm-10">
+            <input defaultValue={product.data.vəzifəÖhdəlikləri} onChange={(e) => handle(e)} value={data.value} type="mail" required class="form-control" id="vəzifəÖhdəlikləri" placeholder="vəzifəÖhdəlikləri" />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="inputEmail" class="col-sm-2 col-form-label">tələblər</label>
+          <div class="col-sm-10">
+            <input defaultValue={product.data.tələblər} onChange={(e) => handle(e)} value={data.value} type="mail" required class="form-control" id="tələblər" placeholder="vəzifəÖhdəlikləri" />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="inputEmail" class="col-sm-2 col-form-label">salary</label>
+          <div class="col-sm-10">
+            <input defaultValue={product.data.salary} onChange={(e) => handle(e)} value={data.value} type="text" required class="form-control" id="salary" placeholder="salary" />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="inputEmail" class="col-sm-2 col-form-label">deadline</label>
+          <div class="col-sm-10">
+            <input defaultValue={product.data.deadline} onChange={(e) => handle(e)} value={data.value} type="date" required class="form-control" id="deadline" placeholder="deadline" />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="inputEmail" class="col-sm-2 col-form-label">City İd</label>
+          <div class="col-sm-10">
+            <input defaultValue={product.data.cityId} onChange={(e) => handle(e)} value={data.value} type="text" required class="form-control" id="cityId" placeholder="City İd " />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="inputEmail" class="col-sm-2 col-form-label">categoryId</label>
+          <div class="col-sm-10">
+            <input defaultValue={product.data.categoryId} onChange={(e) => handle(e)} value={data.value} type="text" required class="form-control" id="categoryId" placeholder=" categoryId " />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="inputEmail" class="col-sm-2 col-form-label">companyId</label>
+          <div class="col-sm-10">
+            <input defaultValue={product.data.companyId} onChange={(e) => handle(e)} value={data.value} type="text" required class="form-control" id="companyId" placeholder="companyId " />
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-10 offset-sm-2">
+            <button type="submit" style={{ 'background-color': '#785BF4', "outline": 'none', 'border': 'none' }} class="btn btn-primary">Əlavə Et</button>
+          </div>
+        </div>
+      </form>
+
+  }
+
   return (
     <div>
       <ShowOnAdmin>
@@ -71,75 +188,7 @@ const VacancyUpdate = (props) => {
               <Sidebar />
               <div class="col py-3">
                 <h1 className="mb-4">Yeniləmək</h1>
-                <form encType='multipart/formdata' onSubmit={(e) => submit(e)}>
-                  {/* {loading && <Loader />} */}
-
-
-                 
-                <input defaultValue={data.id} onChange={(e) => handle(e)} value={data.value} style={{'display':'none'}} type="name" required class="form-control" id="id" placeholder="Ad" />
-                 
-                  <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">Ad</label>
-                    <div class="col-sm-10">
-                      <input defaultValue={data.name} onChange={(e) => handle(e)} value={data.value} type="name" required class="form-control" id="name" placeholder="Ad" />
-                    </div>
-                  </div>
-
-                  <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">typeOfwork</label>
-                    <div class="col-sm-10">
-                      <input defaultValue={data.typeOfwork} onChange={(e) => handle(e)} value={data.value} type="name" required class="form-control" id="typeOfwork" placeholder="typeOfwork" />
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">vəzifəÖhdəlikləri</label>
-                    <div class="col-sm-10">
-                      <input defaultValue={data.vəzifəÖhdəlikləri} onChange={(e) => handle(e)} value={data.value} type="mail" required class="form-control" id="vəzifəÖhdəlikləri" placeholder="vəzifəÖhdəlikləri" />
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">tələblər</label>
-                    <div class="col-sm-10">
-                      <input defaultValue={data.tələblər} onChange={(e) => handle(e)} value={data.value} type="mail" required class="form-control" id="tələblər" placeholder="vəzifəÖhdəlikləri" />
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">salary</label>
-                    <div class="col-sm-10">
-                      <input defaultValue={data.salary} onChange={(e) => handle(e)} value={data.value} type="text" required class="form-control" id="salary" placeholder="salary" />
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">deadline</label>
-                    <div class="col-sm-10">
-                      <input defaultValue={data.deadline} onChange={(e) => handle(e)} value={data.value} type="date" required class="form-control" id="deadline" placeholder="deadline" />
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">City İd</label>
-                    <div class="col-sm-10">
-                      <input defaultValue={data.cityId} onChange={(e) => handle(e)} value={data.value} type="text" required class="form-control" id="cityId" placeholder="City İd " />
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">categoryId</label>
-                    <div class="col-sm-10">
-                      <input defaultValue={data.categoryId} onChange={(e) => handle(e)} value={data.value} type="text" required class="form-control" id="categoryId" placeholder=" categoryId " />
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">companyId</label>
-                    <div class="col-sm-10">
-                      <input defaultValue={data.companyId} onChange={(e) => handle(e)} value={data.value} type="text" required class="form-control" id="companyId" placeholder="companyId " />
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="col-sm-10 offset-sm-2">
-                      <button type="submit" style={{ 'background-color': '#785BF4', "outline": 'none', 'border': 'none' }} class="btn btn-primary">Əlavə Et</button>
-                    </div>
-                  </div>
-                </form>
+{content}
               </div>
             </div>
           </div>
