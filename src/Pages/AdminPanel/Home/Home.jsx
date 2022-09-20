fbaf55102f97a1d0ea3412252e5597b2470cf250
Arif Babayev <arifrb@code.edu.az>
2022-09-20 21:56:22 +0400

    Give sytles to Admin HomePage

--- src/Pages/AdminPanel/Home/Home.jsx
+++ src/Pages/AdminPanel/Home/Home.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { auth } from '../../../Components/Auth/Firebase/config';
+import { REMOVE_ACTIVE_USER, SET_ACTIVE_USER } from '../../../Redux/Slice/authSlice';
 
 const Home = () => {
+
+  const [displayname, setDisplayName] = useState("");
+  const dispatch = useDispatch()
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // const uid = user.uid;
+        if (user.displayName == null) {
+          const u1 = user.email.substring(0, user.email.indexOf("@"));
+          const uName = u1.charAt(0).toUpperCase() + u1.slice(1);
+          // console.log(uName);
+          setDisplayName(uName);
+
+        } else {
+          setDisplayName(user.displayName)
+        }
+        dispatch(SET_ACTIVE_USER({
+          email: user.email,
+          userName: user.displayName ? user.displayName : displayname,
+          userId: user.uid,
+        }))
+      } else {
+        setDisplayName("")
+        dispatch(REMOVE_ACTIVE_USER())
+      };
+    })
+  }, [dispatch, displayname]);
   return (
     <div>
-      <h1>Salam Admin!</h1>
+      <div class="row">
+        <div class="col-sm-5">
+          <div class="box orange">
+            <div className='row'>
+              <div className='col-lg-9'>
+                <h2>Xoş Gəldin!</h2>
+                <p>Salam {displayname}!</p>
+                <p>Işlərində bol səbr və uğurlar!</p>
+
+              </div>
+              <div className='col-lg-3 d-flex align-items-center'>
+
+                <img className='imgss' src="https://assets.codepen.io/2301174/icon-karma.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="box orange">
+            <div className='row'>
+
+              <div className='col-lg-9'>
+                <h2>Xoş Gəldin!</h2>
+                <p>Salam {displayname}!</p>
+                <p>Işlərində bol səbr və uğurlar!</p>
+
+              </div>
+              <div className='col-lg-3 d-flex align-items-center'>
+
+                <img className='imgss' src="https://assets.codepen.io/2301174/icon-karma.svg" alt="" />
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="row2-container d-flex">
+
+
+
+      </div>
+
+
     </div>
   )
 }
