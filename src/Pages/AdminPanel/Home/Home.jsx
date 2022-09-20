8d1e20182fea5a49090f6c4425c5f448854bd7d2
Arif Babayev <arifrb@code.edu.az>
2022-09-21 00:19:18 +0400

    Try to get Gmail Api

--- src/Pages/AdminPanel/Home/Home.jsx
+++ src/Pages/AdminPanel/Home/Home.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { auth } from '../../../Components/Auth/Firebase/config';
 import { REMOVE_ACTIVE_USER, SET_ACTIVE_USER } from '../../../Redux/Slice/authSlice';
+import Clock from 'react-clock';
 
 const Home = () => {
 
@@ -32,6 +33,8 @@ const Home = () => {
       };
     })
   }, [dispatch, displayname]);
+
+  
   return (
     <div>
       <div class="row">
@@ -56,14 +59,22 @@ const Home = () => {
             <div className='row'>
 
               <div className='col-lg-9'>
-                <h2>Xoş Gəldin!</h2>
-                <p>Salam {displayname}!</p>
-                <p>Işlərində bol səbr və uğurlar!</p>
+                <h2>Səlahiyyətlər</h2>
+                <p>Admin olaraq aşağıdakı səlahiyyətlərə sahibsiniz:</p>
+                <li className='li'>Şirkət Yaratmaq,Yeniləmək,Silmək</li>
+                <li className='li'>Vakansiya Yaratmaq,Yeniləmək,Silmək</li>
+                <li className='li'>Kateqoriya Yaratmaq,Yeniləmək,Silmək</li>
+                <li className='li'>Müraciətləri görmək və idarə etmək</li>
+
+
+
+                
+                
+
 
               </div>
               <div className='col-lg-3 d-flex align-items-center'>
-
-                <img className='imgss' src="https://assets.codepen.io/2301174/icon-karma.svg" alt="" />
+                <img className='imgss' style={{"width":'90px'}} src="https://cdn3d.iconscout.com/3d/premium/thumb/transaction-4721271-3927984.png" alt="" />
               </div>
             </div>
           </div>
