68aa77823a82a8a3ee24334591f7417d967def83
Arif Babayev <arifrb@code.edu.az>
2022-09-21 09:30:33 +0400

    Remove unused components

--- src/Pages/AdminPanel/Home/Home.jsx
+++ src/Pages/AdminPanel/Home/Home.jsx
@@ -1,105 +1,38 @@
-import { onAuthStateChanged } from 'firebase/auth';
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { auth } from '../../../Components/Auth/Firebase/config';
-import { REMOVE_ACTIVE_USER, SET_ACTIVE_USER } from '../../../Redux/Slice/authSlice';
-import Clock from 'react-clock';
+import React from 'react';
+import SubmitForm from './SubmitForm';
+import TasksList from './TaskList';
+import './TaskList.css'
 
-const Home = () => {
+class Home extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={tasks: []}
 
-  const [displayname, setDisplayName] = useState("");
-  const dispatch = useDispatch()
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // const uid = user.uid;
-        if (user.displayName == null) {
-          const u1 = user.email.substring(0, user.email.indexOf("@"));
-          const uName = u1.charAt(0).toUpperCase() + u1.slice(1);
-          // console.log(uName);
-          setDisplayName(uName);
+    this.addTask = this.addTask.bind(this);
+    this.removeTask = this.removeTask.bind(this);
+  }
 
-        } else {
-          setDisplayName(user.displayName)
-        }
-        dispatch(SET_ACTIVE_USER({
-          email: user.email,
-          userName: user.displayName ? user.displayName : displayname,
-          userId: user.uid,
-        }))
-      } else {
-        setDisplayName("")
-        dispatch(REMOVE_ACTIVE_USER())
+  addTask(task){
+    this.setState((prevState) => {
+      return { 
+        tasks: prevState.tasks.concat(task) 
       };
-    })
-  }, [dispatch, displayname]);
+    });
+  }
 
-  
-  return (
-    <div>
-      <div class="row">
-        <div class="col-sm-5">
-          <div class="box orange">
-            <div className='row'>
-              <div className='col-lg-9'>
-                <h2>Xoş Gəldin!</h2>
-                <p>Salam {displayname}!</p>
-                <p>Işlərində bol səbr və uğurlar!</p>
+  removeTask(id){
+      const tasks = this.state.tasks.filter(element => (element.id !== id));
+      this.setState({ tasks: tasks });
+  }
 
-              </div>
-              <div className='col-lg-3 d-flex align-items-center'>
-
-                <img className='imgss' src="https://assets.codepen.io/2301174/icon-karma.svg" alt="" />
-              </div>
-            </div>
-          </div>
-
-          <div class="box orange">
-            <div className='row'>
-              <div className='col-lg-9'>
-                <h2>Xoş Gəldin!</h2>
-                <p>Salam {displayname}!</p>
-                <p>Işlərində bol səbr və uğurlar!</p>
-
-              </div>
-              <div className='col-lg-3 d-flex align-items-center'>
-
-                <img className='imgss' src="https://assets.codepen.io/2301174/icon-karma.svg" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-5">
-          <div class="box orange">
-            <div className='row'>
-
-              <div className='col-lg-9'>
-                <h2>Səlahiyyətlər</h2>
-                <p>Admin olaraq aşağıdakı səlahiyyətlərə sahibsiniz:</p>
-                <li className='li'>Şirkət Yaratmaq,Yeniləmək,Silmək</li>
-                <li className='li'>Vakansiya Yaratmaq,Yeniləmək,Silmək</li>
-                <li className='li'>Kateqoriya Yaratmaq,Yeniləmək,Silmək</li>
-                <li className='li'>Müraciətləri görmək və idarə etmək</li>
-
-
-              </div>
-              <div className='col-lg-3 d-flex align-items-center'>
-                <img className='imgss' style={{"width":'90px'}} src="https://cdn3d.iconscout.com/3d/premium/thumb/transaction-4721271-3927984.png" alt="" />
-              </div>
-            </div>
-          </div>
-
-        </div>
+  render(){
+    return (
+      <div className="App">
+        <SubmitForm addTask={this.addTask}/>
+        <TasksList tasks={this.state.tasks} removeTask={this.removeTask}/>
       </div>
-      <div class="row2-container d-flex">
-
-
-
-      </div>
-
-
-    </div>
-  )
+    );
+  }
 }
 
-export default Home
+export default Home;
