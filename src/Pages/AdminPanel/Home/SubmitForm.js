bc1410d003990625ab71307619b8b46bd704151d
Arif Babayev <arifrb@code.edu.az>
2022-09-21 09:23:54 +0400

    Tasklist created

---
+++ src/Pages/AdminPanel/Home/SubmitForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SubmitForm.css';
+class SubmitForm extends React.Component{
+    
+    render(){
+        return (
+            <div className='form'>
+                <form>
+                    <input className='input' placeholder='Add Task' />
+                    <button className='add-button' type='submit'>Add</button>
+                </form>
+            </div>
+        );
+    }
+}
+export default SubmitForm;
