57052c658b8fb7a840cae0a1bfc00174de330089
Arif Babayev <arifrb@code.edu.az>
2022-09-27 17:38:15 +0400

    File upload tests succesfulls

--- src/Pages/AdminPanel/CRUD/Company/CompanyImage.jsx
+++ src/Pages/AdminPanel/CRUD/Company/CompanyImage.jsx
@@ -1,26 +1,53 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+  list,
+} from "firebase/storage";
+import { v4 } from "uuid";
+import { storage } from "../../../../Components/Auth/Firebase/config";
 
-export default function CompanyAdd() {
-    const [images, setImages] = useState([]);
-    const [imageURLs, setImageURLs] = useState([]);
+function CompanyImage() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
 
-    useEffect(() => {
-        if (images.length < 1) return; 
-        const newImageUrls = [];
-        images.forEach(image => newImageUrls.push(URL.createObjectURL(image)));
-        setImageURLs(newImageUrls);
-    }, [images]);
+  const imagesListRef = ref(storage, "images/");
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
 
-    function onImageChange(e) {
-        setImages([...e.target.files]);
-    }
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
 
-    return (
-        <>
-        <h1>sa</h1>
-            <input type='file' multiple accept='image/*' onChange={onImageChange} />
-            { imageURLs.map(imageSrc => <img alt='value' src={imageSrc} />) }
-        </>
-    )
+  return (
+    <div className="App">
+      <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadFile}> Upload Image</button>
+      {imageUrls.map((url) => {
+        return <img src={url} />;
+      })}
+    </div>
+  );
 }
 
+export default CompanyImage;
