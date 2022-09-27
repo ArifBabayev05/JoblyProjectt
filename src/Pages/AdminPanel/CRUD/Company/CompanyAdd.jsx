d428357ae0d7d7a62b5fc1bcc79696737d9d8de8
Arif Babayev <arifrb@code.edu.az>
2022-09-28 00:38:17 +0400

    Upload Image Doesnt work

--- src/Pages/AdminPanel/CRUD/Company/CompanyAdd.jsx
+++ src/Pages/AdminPanel/CRUD/Company/CompanyAdd.jsx
@@ -18,7 +18,7 @@ import { storage } from '../../../../Components/Auth/Firebase/config'
 
 const CompanyAdd = () => {
     const navigate = useNavigate();
-    console.log(new Date().toJSON());
+    // console.log(new Date().toJSON());
     const [selectedFile, setSelectedFile] = useState('')
     const [loading, setLoading] = useState(false)
     const url = 'http://localhost:53410/api/Company/add';
@@ -26,42 +26,26 @@ const CompanyAdd = () => {
         name: "",
         mail: "",
         telNumber: "",
-        imageId: 1,
-        path: "",
+        imageId: "",
+        ImageFile: "",
         createdDate: ""
 
     })
-    const handleInputChange = (e) => {
-        setSelectedFile(e.target.value);
-        setLoading(false);
-        const newData = { ...data }
-        newData[e.target.files[0]] = e.target.value;
-        setData(newData);
-    }
-    function submits() {
-        const data = new FormData()
-        data.append('file', selectedFile)
-        console.warn(selectedFile);
-        axios.post("http://localhost:53410/api/Company/fileUpload", data, { // receive two parameter endpoint url ,form data 
-        })
-            .then(res => { // then print response status
-                console.warn(res);
-            })
-
-
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data'
+        }
     }
     function submit(e) {
         e.preventDefault();
         setLoading(false);
-        axios.post(url, {
+        axios.post(url,config, {
             name: data.name,
             mail: data.mail,
             telNumber: data.telNumber,
-            // imageId: parseInt(data.imageId),
-            imageId: data.imageId,
-            path: data.path,
-
-
+            imageId: parseInt(data.imageId),
+            ImageFile: data.ImageFile,
+            // imageId: data.imageId,
             createdDate: new Date().toJSON()
         }).then(res => {
             setLoading(false);
@@ -79,46 +63,11 @@ const CompanyAdd = () => {
         newData[e.target.id] = e.target.value;
         setData(newData);
     }
-    // const [imageUpload, setImageUpload] = useState(null);
-    // const [imageUrls, setImageUrls] = useState([]);
-
-    // const imagesListRef = ref(storage, "images/");
-    // const uploadFile = () => {
-    //     if (imageUpload == null) return;
-    //     const imageRef = ref(storage, `images/${data.createdDate}`);
-    //     uploadBytes(imageRef, imageUpload).then((snapshot) => {
-    //         getDownloadURL(snapshot.ref).then((url) => {
-    //             setImageUrls((prev) => [...prev, url]);
-    //         });
-    //     });
-    // };
-
-    // useEffect(() => {
-    //     listAll(imagesListRef).then((response) => {
-    //         response.items.forEach((item) => {
-    //             getDownloadURL(item).then((url) => {
-    //                 setImageUrls((prev) => [...prev, url]);
-    //             });
-    //         });
-    //     });
-    // }, []);
-    //Category Option
-    const [categoryDatas, setCategoryDatas] = useState([])
-    const formData = new FormData(); formData.append("file", categoryDatas[0]);
-    const config = {
-        headers: { 'content-type': 'multipart/form-data' }
-    }
-    useEffect(() => {
-        axios.post('http://localhost:53410/api/Company/fileUpload/', config)
-            .then(res => {
-                setCategoryDatas(res.data)
-            }).catch(err => console.log(err))
-    }, []);
     function handles(e) {
-        const newData = { ...categoryDatas }
-        console.log(e.taget.value)
-        newData[e.target.selected] = e.target.value;
-        setCategoryDatas(newData);
+        setLoading(false);
+        const newData = { ...data }
+        newData[e.target.id] = e.target.files;
+        setData(newData);
     }
 
 
@@ -160,15 +109,13 @@ const CompanyAdd = () => {
                                     <div className="row mb-3">
                                         <label for="inputEmail" className="col-sm-2 col-form-label">İmage upload</label>
                                         <div className="col-sm-10">
-                                            <input onChange={(e) => handleInputChange(e)} value={data.value} accept='image/*' type="file" className="form-control" id="path" placeholder="Path" />
-                                            <button type="submit" className="btn btn-dark" onClick={() => submits()}>Əlavə Et</button>
-
+                                            <input onChange={(e) => handle(e)} value={data.value}  accept='image/*' type="file" className="form-control" id="ImageFile" placeholder="Path" />
                                         </div>
                                     </div>
 
                                     <div className="row">
                                         <div className="col-sm-10 offset-sm-2">
-                                            <button type="submit" onMouseEnter={(e) => handles(e)} value={data.value} id='imageId' style={{ 'background-color': '#785BF4', "outline": 'none', 'border': 'none' }} className="btn btn-primary">Əlavə Et</button>
+                                            <button type="submit" onMouseEnter={(e) => handle(e)} value={data.value} id='imageId' style={{ 'background-color': '#785BF4', "outline": 'none', 'border': 'none' }} className="btn btn-primary">Əlavə Et</button>
                                         </div>
                                     </div>
                                 </form>
--- src/Pages/AdminPanel/CRUD/Company/CompanyImage.jsx
+++ src/Pages/AdminPanel/CRUD/Company/CompanyImage.jsx
@@ -1,172 +1,53 @@
-// import React, { useState, useEffect } from 'react'
-
-// import axios from 'axios'
-// import '../../Admin.css'
-// import { Link } from 'react-router-dom'
-// import { toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import { v4 } from "uuid";
-// import {
-//     ref,
-//     uploadBytes,
-//     getDownloadURL,
-//     listAll,
-//     list,
-// } from "firebase/storage";
-// import Loader from '../../../../Components/Jobs/Loader'
-// import { ShowOnAdmin, ShowOnUser } from '../../../../Layouts/HiddenLinks/Router'
-// import Sidebar from '../../Sidebar/Sidebar'
-// import { storage } from '../../../../Components/Auth/Firebase/config';
-
-
-// const CompanyAdmin = (props) => {
-//     const [imageUpload, setImageUpload] = useState(null);
-//     const [imageUrls, setImageUrls] = useState([]);
-
-//     const imagesListRef = ref(storage, "images/");
-//     const uploadFile = () => {
-//         if (imageUpload == null) return;
-//         const imageRef = ref(storage, `images/${imageUpload.name + data.id}`);
-//         uploadBytes(imageRef, imageUpload).then((snapshot) => {
-//             getDownloadURL(snapshot.ref).then((url) => {
-//                 setImageUrls((prev) => [...prev, url]);
-//             });
-//         });
-//     };
-
-    
-//     const [query, setQuery] = useState("")
-//     const [data, setData] = useState([])
-//     useEffect(() => {
-//         axios.get('http://localhost:53410/api/Company/getall')
-
-//             .then(res => {
-//                 setData(res.data)
-//             }).catch(err => console.log(err))
-//     }, [])
-
-//     function Update(id) {
-//         console.log(id);
-//         props.history.push("/company" + id)
-//         // navigate("/companyupdate")
-
-//     }
-//     const Delete = (id, e) => {
-//         const url = `http://localhost:53410/api/Company/delete?id=${id}`
-//         console.log(id);
-//         e.preventDefault();
-
-//         axios.post(url)
-//             .then(res => {
-//                 toast.success("Uğurla silindi")
-//                 console.log(res.data)
-//             }).catch(err => toast.error(err))
-//     }
-//     useEffect(() => {
-//         listAll(imagesListRef).then((response) => {
-//             response.items.forEach((item) => {
-//                 getDownloadURL(item).then((url) => {
-//                     setImageUrls((prev) => [...prev, url]);
-//                 });
-//             });
-//         });
-//     }, []);
-//     const array = data.filter((value) => {
-//         if (query === "") {
-//             return value;
-//         }
-//         else if (value.name.toLowerCase().includes(query.toLowerCase())) {
-//             return value;
-//         }
-//         else if (value.telNumber.toLowerCase().includes(query.toLowerCase())) {
-//             return value;
-//         }
-//         else if (value.mail.toLowerCase().includes(query.toLowerCase())) {
-//             return value;
-//         }
-//     }).map((data, index) => {
-//         return (
-//             <tr>
-//                 <td>{data.id}</td>
-//                 {imageUrls.map((url) => {
-//                     <td><img alt='value' style={{ 'width': '35px', 'height': '35px', 'border-radius': '50%' }} className='me-3' src={url} /> {data.name}</td>
-//                 })}
-//                 {imageUrls.map((url) => {
-//                     return <td><img src={url} alt='value' style={{'width': '60px'}} />{data.name}</td>
-//                 })}
-//                 {/* <td><img alt='value' style={{ 'width': '35px', 'height': '35px', 'border-radius': '50%' }} className='me-3' src={data.path} /> {data.name}</td> */}
-//                 <td >+{data.telNumber}</td>
-//                 <td>{data.mail}</td>
-//                 <td>{data.createdDate.slice(0, 10)}</td>
-//                 {/* <td>{data.createdDate}</td> */}
-
-
-//                 <td><Link to={`/companyupdate/${data.id}`} onClick={() => Update(data.id)} className='btn text-white btn-info update'>Yenilə</Link></td>
-//                 <td><button onClick={(e) => Delete(data.id, e)} className='btn btn-danger delete'>Sil</button></td>
-
-
-//             </tr>
-//         )
-//     })
-
-
-//     return (
-//         <div>
-//             <ShowOnAdmin>
-//                 <div>
-//                     <div className="container-fluid">
-//                         <div className="row flex-nowrap">
-//                             <Sidebar />
-//                             <div className="col py-3">
-//                                 <div className='row'>
-//                                     <div className='col-md-9 col-sm-6 col-lg-12 d-flex mb-3 justify-content-between'>
-//                                         <h3>Şirkətlər</h3>
-//                                         <form class="search-box newSearchInputForm" style={{ 'margin-right': "60px" }}>
-//                                             <input onChange={(event) => setQuery(event.target.value)} type="text" placeholder="Axtarış hissəsi" />
-//                                             <button type="reset"></button>
-//                                         </form>
-//                                     </div>
-
-//                                     <div className='d-flex mb-4 mt-2'>
-//                                         <a href='admin/companyadd' className='btn btn-success position-relative'>Şirkət Əlavə Et</a>
-//                                     </div>
-//                                 </div>
-//                                 <div style={{ 'overflow-x': 'auto' }}>
-//                                     <table>
-//                                         <tr>
-//                                             <th>Id</th>
-//                                             <th>Adı</th>
-//                                             <th>Telefon Nömrəsi</th>
-//                                             <th>Email Ünvanı</th>
-//                                             <th>Yaradılma Tarixi</th>
-//                                             <th className='text-info'>Yəniləmək</th>
-//                                             <th className='text-danger'>Silmək</th>
-//                                         </tr>
-
-//                                         {array}
-//                                     </table>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </ShowOnAdmin>
-//             <ShowOnUser>
-//                 <div>
-//                     <Loader />
-//                 </div>
-//             </ShowOnUser>
-//         </div>
-//     )
-// }
-
-// export default CompanyAdmin
 import React from 'react'
+import axios from 'axios';
 
-const CompanyImage = () => {
-  return (
-    <div>CompanyImage</div>
-  )
+class CompanyImage extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            selectedFile: '',
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        this.setState({
+            selectedFile: event.target.files[0],
+        })
+    }
+
+    submit() {
+        const data = new FormData()
+        data.append('file', this.state.selectedFile)
+        console.warn(this.state.selectedFile);
+        let url = "http://localhost:53410/api/Company/add";
+
+        axios.post(url, data, { // receive two parameter endpoint url ,form data 
+        })
+            .then(res => { // then print response status
+                console.warn(res);
+            })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="">
+
+                        <input type="file" className="form-control" name="ImageFile" onChange={this.handleInputChange} />
+
+                        <button type="submit" className="btn btn-dark" onClick={() => this.submit()}>Save</button>
+
+
+                    </div>
+                </div>
+            </div >
+        )
+    }
 }
 
-export default CompanyImage
+export default CompanyImage;
