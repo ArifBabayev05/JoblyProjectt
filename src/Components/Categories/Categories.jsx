bb0284b7ac8e0603a963028ff61f319a023d9f10
Arif Babayev <arifrb@code.edu.az>
2022-10-03 14:58:25 +0400

    Category Details coding

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -3,103 +3,103 @@ import React from 'react'
 const Categories = () => {
     const url = `http://localhost:53410/api/Company/getall`
     const [query, setQuery] = useState("")
-  
-  
+
+
     const [products, setProducts] = useState({
-      loading: false,
-      data: null,
-      error: false
-    })
-  
-  
-  
-    useEffect(() => {
-      setProducts({
-        loading: true,
+        loading: false,
         data: null,
         error: false
-  
-      })
-  
-      axios.get(url)
-        .then(response => {
-          setProducts({
-            loading: false,
-            data: response.data,
+    })
+
+
+
+    useEffect(() => {
+        setProducts({
+            loading: true,
+            data: null,
             error: false
-          })
-  
-            .catch(() => {
-              setProducts({
-                loading: false,
-                data: null,
-                error: true
-              })
-            })
+
         })
+
+        axios.get(url)
+            .then(response => {
+                setProducts({
+                    loading: false,
+                    data: response.data,
+                    error: false
+                })
+
+                    .catch(() => {
+                        setProducts({
+                            loading: false,
+                            data: null,
+                            error: true
+                        })
+                    })
+            })
     }, [url])
-  
+
     let content = null
-  
+
     if (products.loading) {
-      content = <Loader />
+        content = <Loader />
     }
-  
+
     if (products.error) {
-      content = <p>Xəta baş verdi, yenidən yoxlayın.</p>
+        content = <p>Xəta baş verdi, yenidən yoxlayın.</p>
     }
-  
+
     if (products.data) {
-      content =
-        products.data.map((product) =>
-          <div key={product.id}>
-            <JobCard product={product} />
-          </div>
-        )
-  
+        content =
+            products.data.map((product) =>
+                <div key={product.id}>
+                    <JobCard product={product} />
+                </div>
+            )
+
     }
-  
+
     if (products.data) {
-      content =
-        products.data.filter(product => {
-          if (query === "") {
-  
-            return product;
-  
-          } else if (product.name.toLowerCase().includes(query.toLowerCase())) {
-  
-            return product;
-          }
-          // else if (product.company.name.toLowerCase().includes(query.toLowerCase())) {
-  
-          //   return product;
-          // }
-        }).map((product) =>
-          <div key={product.id}>
-            <JobCard product={product} />
-          </div>
-        ).reverse();
+        content =
+            products.data.filter(product => {
+                if (query === "") {
+
+                    return product;
+
+                } else if (product.name.toLowerCase().includes(query.toLowerCase())) {
+
+                    return product;
+                }
+                // else if (product.company.name.toLowerCase().includes(query.toLowerCase())) {
+
+                //   return product;
+                // }
+            }).map((product) =>
+                <div key={product.id}>
+                    <JobCard product={product} />
+                </div>
+            ).reverse();
     }
-  
+
     return (
-      <div>
-        <div className='d-flex container'>
-          <h1 className='text container ' style={{ color: 'var(--pink)', fontWeight: 'normal', fontSize: '35px', alignItems: 'center', display: 'flex' }}>Şirkətlər</h1>
-          <form className='searchJob mb-5' style={{ alignItems: 'center', display: 'flex', top: '20px' }}>
-            <input className='searchBar mt-2 p-2' onChange={event => setQuery(event.target.value)} type='text'></input>
-            <button className='search__submit' type='submit'>
-              <img src={src} alt='some value' />
-            </button>
-            {/* <button onClick={()=>sorting("salary")}>Maaşa görə sırala</button> */}
-          </form>
+        <div>
+            <div className='d-flex container'>
+                <h1 className='text container ' style={{ color: 'var(--pink)', fontWeight: 'normal', fontSize: '35px', alignItems: 'center', display: 'flex' }}>Şirkətlər</h1>
+                <form className='searchJob mb-5' style={{ alignItems: 'center', display: 'flex', top: '20px' }}>
+                    <input className='searchBar mt-2 p-2' onChange={event => setQuery(event.target.value)} type='text'></input>
+                    <button className='search__submit' type='submit'>
+                        <img src={src} alt='some value' />
+                    </button>
+                    {/* <button onClick={()=>sorting("salary")}>Maaşa görə sırala</button> */}
+                </form>
+            </div>
+
+            <div className='row row-cols-1 row-cols-md-3 g-4 mx-5'>
+                {content}
+            </div>
         </div>
-  
-        <div className='row row-cols-1 row-cols-md-3 g-4 mx-5'>
-              {content}
-          </div>
-      </div>
     )
-  }
+
 }
 
 export default Categories
