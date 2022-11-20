4cb87b7cf3d0aa376a0102ca71e1ed7a53e47c8f
Arif Babayev <arifrb@code.edu.az>
2022-11-20 19:37:47 +0400

    test

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,17 @@ import "./Assets/Styles/index.css"
 import store from "./Redux/store"
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-// import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    
+  <HashRouter>
+    <Provider store={store}>
       <App />
-    
-  </Provider>
+    </Provider>
+  </HashRouter>
 );
 
 reportWebVitals();
